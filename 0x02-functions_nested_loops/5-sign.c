796a2885ab06459d8d3b05d00be83c0e277159aa
MOOONESS <anasboughanmi2@gmail.com>
2023-06-20 16:01:37 -0700

    ee

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,19 +8,19 @@ int print_sign(int n);
 {
 if (n < 0)
 {
-_putchar(45);
+_putchar('-');
 _putchar('\n');
 return (-1);
 }
 else if (n > 0)
 {
-_putchar(48);
+_putchar('+');
 _putchar('\n');
 return (1);
 }
 else
 {
-_putchar(43);
+_putchar(48);
 _putchar('\n');
 return (0);
 }
