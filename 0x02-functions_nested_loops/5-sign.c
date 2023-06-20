7c73ff92cef771f49a27a157cd4c85cc41f39d34
MOOONESS <anasboughanmi2@gmail.com>
2023-06-20 16:06:35 -0700

    aaaaaaaaaa

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,24 @@
 #include "main.h"
 /**
- *print_sign -putcha
+ *print_sign - putcha
  *Return:0
  *@n:var
 **/
-int print_sign(int n);
+int print_sign(int n)
 {
 if (n < 0)
 {
 _putchar('-');
-_putchar('\n');
 return (-1);
 }
 else if (n > 0)
 {
 _putchar('+');
-_putchar('\n');
 return (1);
 }
 else
 {
 _putchar(48);
-_putchar('\n');
 return (0);
 }
 }
