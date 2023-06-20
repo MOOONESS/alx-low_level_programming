37384c09bc1377a63b71e3acdef0dd645c644741
MOOONESS <anasboughanmi2@gmail.com>
2023-06-20 15:01:28 -0700

    ee

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- *print_alphabet -putcha
+ *print_alphabet_x10 -putcha
  *Return:0
  **/
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char c;
 int i;
 for (i = 0; i < 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
+{
 _putchar(c);
+}
 _putchar('\n');
 }
 }
